760271cd3048e1518e35337368a7ed310d823964
Shamik Kumar De <shamikisbatman@gmail.com>
2024-02-08 19:40:44 +0530

    Mining, Receiving and Validation of Blocks

--- sim.py
+++ sim.py
@@ -21,7 +21,7 @@ class Peer:
         self.transactions = []
         self.used_txns = []
         self.speed_of_light_delay=speed_of_light_delay
-        self.blockchain=Blockchain(len(self.all_peers))
+        self.blockchain=None
         self.hashing_power=hashing_power
         if not is_low_cpu:
             self.hashing_power*=10
@@ -29,10 +29,11 @@ class Peer:
     
     def store_all_peers(self, all_peer_ids):
         self.all_peers=all_peer_ids
+        self.blockchain=Blockchain(len(self.all_peers))
 
     def generate_transaction(self, current_time):
         receiver_id=random.choice([peer_id for peer_id in self.all_peers if peer_id != self.peer_id])
-        amount = random.randint(0, 20)
+        amount = random.randint(0, 5)
         txn=Transaction(self.peer_id, receiver_id, amount)
         self.receive_transaction(txn, current_time)
     
@@ -61,25 +62,74 @@ class Peer:
     def generate_block(self, current_time):
         prev_hash=hash(self.blockchain.last_block)
         index=self.blockchain.last_block.index+1
-        new_block=Block(prev_hash, current_time, self.peer_id, index)
+        new_block=Block(prev_hash, current_time, self.peer_id, index, self.blockchain.last_block.blk_id)
+        
         i=0
         while i < len(self.transactions) and sys.getsizeof(new_block)<1000000:
             new_block.add_transaction(self.transactions[i])
             i+=1
         Tk = np.random.exponential(self.mean_block_generation_time/self.hashing_power)
-        mining = Event(current_time+Tk, 'blk_mining', self.peer_id, (new_block, self.blockchain.last_block))
+        mining = Event(current_time+Tk, 'blk_mining', self.peer_id, new_block)
         heapq.heappush(event_queue, mining)
 
+    def mine_block(self, current_time, data):
+        block = data
+        if block.prev_blk_id!=self.blockchain.last_block.blk_id:
+            self.generate_block(current_time)
+        else:
+            self.receive_block(current_time, block)
+
+    def validate_block(self, block):
+        if block.index-1 in self.blockchain.blocks:
+            prev_block=None
+            for blk in self.blockchain.blocks[block.index-1]:
+                if blk.blk_id == block.prev_blk_id:
+                    prev_block=blk
+                    if hash(blk) != block.prev_hash:
+                        return None
+            if not prev_block:
+                return None
+        else:
+            return None
+        balance=prev_block.balance[:]
+        for txn in block.transactions:
+            if isinstance(txn, Coinbase):
+                balance[txn.miner]+=50
+            else:
+                balance[txn.sender]-=txn.amount
+                balance[txn.receiver]+=txn.amount
+
+        print("Validation", balance)
+        for i in balance:
+            if i < 0:
+                return None
+        
+        return balance
+
     def receive_block(self, current_time, block):
-        # if(block in self.blockchain.blocks):
-        #     return
-        # coinbase=block.transactions[0]
-        # for i in range(1, len(block.transactions)):
-        #     if block.transactions[i] in self.transactions:
-        #         self.transactions.remove(block.transactions[i])
-        #     self.used_txns.append(block.transactions[i])
-        # self.blockchain.add_block(block)
-        pass
+        if block.index in self.blockchain.blocks:
+            for blk in self.blockchain.blocks[block.index]:
+                if blk.blk_id == block.blk_id:
+                    return
+        balance=self.validate_block(block)
+        if not balance:
+            return
+        block.balance=balance
+        print("Vhggyhgy", balance)
+        self.blockchain.add_block(block)
+        m = sys.getsizeof(block)*8
+        for neighbour in self.neighbours:
+            c=0
+            if not self.is_slow and not neighbour[1]:
+                c=100000000
+            else:
+                c=5000000
+            d = np.random.exponential(96000/c)
+            time_delta = self.speed_of_light_delay + (m/c) + d
+            new_event=Event(current_time+time_delta, 'blk_receive', neighbour[0], block)
+            heapq.heappush(event_queue, new_event)
+        self.generate_block(current_time)
+            
 
 class Transaction:
     def __init__(self, peer_id1, peer_id2, amount):
@@ -96,7 +146,7 @@ class Coinbase:
         self.statement=f"{self.txn_id}:{self.miner} mines 50 coins"
 
 class Block:
-    def __init__(self, prev_hash, mine_time, miner_id, index):
+    def __init__(self, prev_hash, mine_time, miner_id, index, prev_blk_id):
         self.blk_id=uuid.uuid4()
         self.prev_hash=prev_hash
         self.mine_time=mine_time
@@ -105,13 +155,14 @@ class Block:
         coinbase=Coinbase(miner_id)
         self.transactions.append(coinbase)
         self.balance=[]
+        self.prev_blk_id=prev_blk_id
 
     def add_transaction(self, txn):
         self.transactions.append(txn)
 
 class Blockchain:
     def __init__(self, num_peers):
-        genesis=Block(0, 0, 0, 0)
+        genesis=Block(0, 0, -1, 0, 0)
         genesis.balance=[50]*num_peers
         self.blocks={}
         self.blocks[0]=[genesis]
@@ -150,7 +201,6 @@ class Simulation:
         self.low_cpu_percentage = low_cpu_percentage
 
     def initialize_peers(self, speed_of_light_delay, mean_block_generation_time):
-        all_peer_ids=[]
         slow=[0]*self.num_peers
         low_cpu=[0]*self.num_peers
         indices1 = random.sample(range(self.num_peers), int(self.num_peers * self.slow_percentage/ 100))
@@ -160,15 +210,13 @@ class Simulation:
         hashing_power=1/(num_high_cpu*10 + num_low_cpu)
         for i in indices1:
             slow[i]=1
-        for i in indices1:
+        for i in indices2:
             low_cpu[i]=1
         for i in range(0, self.num_peers):
-            peer_id = i
-            all_peer_ids.append(peer_id)
-            peer = Peer(peer_id, slow[i], low_cpu[i], speed_of_light_delay, hashing_power, mean_block_generation_time)
+            peer = Peer(i, slow[i], low_cpu[i], speed_of_light_delay, hashing_power, mean_block_generation_time)
             self.peers.append(peer)
         for peer in self.peers:
-            peer.store_all_peers(all_peer_ids)
+            peer.store_all_peers(range(0, len(self.peers)))
 
     def generate_random_topology(self):
         self.graph.add_nodes_from(self.peers)
@@ -228,6 +276,8 @@ class Simulation:
                 peer.generate_block(current_time)
             elif current_event.event_type == 'blk_mining':
                 peer.mine_block(current_time, current_event.data)
+            elif current_event.event_type == 'blk_receive':
+                peer.receive_block(current_time, current_event.data)
 if __name__=="__main__":
     if(len(sys.argv)<7):
         print(f"Usage: {sys.argv[0]} <num_peers> <slow_%> <low_cpu_%> <mean_txn_time> <mean_blkgen_time> <duration>")
@@ -250,7 +300,7 @@ if __name__=="__main__":
     print("Creating network...")
     simulation.generate_random_topology()
     while not simulation.is_connected_graph():
-        recreate_graph()
+        simulation.recreate_graph()
     print("Adding Events to the Queue...")
     simulation.initialize_events(simulation_duration, mean_transaction_time)
     # print(len(event_queue))
@@ -260,6 +310,5 @@ if __name__=="__main__":
     print("Simulation Completed")
 
 
-# 3. Create Block
-# 4. Mine Block
-# 5. Receive Block
+# 1. Longest XChain selection on the basis of arrival time of the blocks
+# 2. Transactions should not be repeated in a in the longest chain
